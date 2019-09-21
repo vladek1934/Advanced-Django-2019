4c4c880435957fbc9e70b9bf7548bd9e05715ba0
vladek1934 <vlad.mun1999@gmail.com>
2019-09-21 12:35:59 +0600

    Update serializers.py

--- Week2/base/serializers.py
+++ Week2/base/serializers.py
@@ -9,5 +9,5 @@ class ProfileSerializer(serializers.ModelSerializer):
         fields = ('id', 'user', 'bio', 'address', 'web_site')
 
     def create(self, validated_data):
-        user = Profile.objects.create_user(**validated_data)
+        user = Profile.objects.create(**validated_data)
         return user
